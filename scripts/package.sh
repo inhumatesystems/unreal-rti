8c50f77e451dd7f6c8194760ceb85e18ee4af03a
Martin Insulander <martin.insulander@inhumatesystems.com>
2024-08-19 20:22:14 +0200

    Add package, publish, release CI jobs

--- scripts/package.sh
+++ scripts/package.sh
@@ -1,26 +1,19 @@
 #!/bin/bash -e
 
-[ -z "$VERSION" ] && VERSION=$CI_COMMIT_TAG
-[ -z "$VERSION" -a ! -z "$CI_PIPELINE_IID" ] && VERSION=0.0.$CI_PIPELINE_IID
 if [ -z "$VERSION" ]; then
     echo "Please set the VERSION environment variable when manually building a release."
     exit 1
 fi
 
-FILENAME=inhumate-unreal-rti-client-$VERSION.zip
+FILENAME=inhumate-rti-unreal-$VERSION.zip
 
-cd "$(dirname $0)/../Plugins"
+cd "$(dirname $0)/.."
 
-if [ "$VERSION" == "0.0.1" -a ! -z "$CI_PIPELINE_ID" ]; then
-    FILENAME=inhumate-unreal-rti-client-$CI_PIPELINE_ID.zip
-    echo "CI pipeline ${CI_PIPELINE_ID}" >InhumateRTI/VERSION.txt
-else
-    echo "${VERSION}" >InhumateRTI/VERSION.txt
-fi
+rm -rf Build
+mkdir -p Build/InhumateRTI
+cp -rfp InhumateRTI.uplugin Content Resources Source ThirdParty Build/InhumateRTI/
+cp -f README-Packaged.md Build/InhumateRTI/README.txt
+sed -i "s/0.0.1-dev-version/${VERSION}/g" Build/InhumateRTI/README.txt Build/InhumateRTI/Source/InhumateRTI/Public/RTISubsystem.h
 
-mkdir -p ../Build
-rm -rf InhumateRTI/Intermediate
-rm -f InhumateRTI/Binaries
-
-zip -r ../Build/$FILENAME *
-ls -lh ../Build/$FILENAME
+cd Build
+zip -r $FILENAME *
